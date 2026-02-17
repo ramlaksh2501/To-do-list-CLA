7c857c146493c039c9c34c706c0bca09cc5beab7
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-02-17 14:26:42 +0530

    added codes

---
+++ app.c
@@ -0,0 +1,21 @@
+/* functionalities:
+add tasks
+delete task  */
+#include<stdio.h>
+#include<stdlib.h>
+FILE *original,*copy;
+original=fopen("original.txt","r+");
+copy=fopen("copy.txt","r+")
+void add();
+void delete(int line);
+void display();
+int main(){
+    printf("\t\t-------------To Do List-------------\n");
+while(1){
+    return 0;
+}
+
+
+
+
+}
---
+++ init.c
@@ -0,0 +1,12 @@
+//to create the files to get the app running 
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+    FILE *copy,*original;
+    if(fopen("./copy.txt","r")!=NULL || fopen("./original.txt","r")!=NULL){
+    copy=fopen("./copy.txt","w");
+    original=fopen("./original.txt","w");
+    fclose(copy);
+    fclose(original);}
+    return 0;
+}
