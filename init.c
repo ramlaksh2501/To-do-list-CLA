dc0e40d7ff9de9ca068a9fdbd45f5a0c3abcbf31
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-02-27 20:48:00 +0530

    added function prototype

--- init.c
+++ init.c
@@ -1,7 +1,12 @@
 //to create the files to get the app running 
 #include<stdlib.h>
 #include<stdio.h>
+void display();
+void add_task();
+void rm_task();
+void close();
 int main(){
+
     FILE *copy,*original;
     if(fopen("./copy.txt","r")!=NULL || fopen("./original.txt","r")!=NULL){
     copy=fopen("./copy.txt","w");
@@ -9,4 +14,5 @@ int main(){
     fclose(copy);
     fclose(original);}
     return 0;
+
 }
