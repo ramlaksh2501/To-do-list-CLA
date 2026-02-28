de88f0cf7a2d61d866e6712c8b1518386ddeccf8
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-02-28 13:16:13 +0530

    added the interface to interact

--- app.c
+++ app.c
@@ -8,22 +8,27 @@ delete task  */
 #include<stdlib.h>
 
 FILE *org;
- FILE *cpy;
+FILE *cpy;
 
 void display(){
+
+    int line=1;
     printf("\t\t***** Tasks you need to complete *****\n");
     char a;
     a=fgetc(org);
     char j=0;
     while(a!=EOF){
         if(j==0){
+            printf("%d:",line);
             printf("\u2610");
             j=1;
         }
         printf("%c",a);
          if(a=='\n'){
+            line++;
+             printf("%d:",line);
             printf("\u2610");
-            
+
         }
         a=fgetc(org);
     }
@@ -34,13 +39,35 @@ void add_task();
 void rm_task();
 void close();
 int main(){
-    
+
 org=fopen("original.txt","r+");
 cpy=fopen("copy.txt","r+");
-    printf("\t\t-------------To Do List-------------\n");
+printf("\t\t-------------To Do List-------------\n");
 while(1){
-    display();
-    return 0;
+    printf("---------------------------------------------\n");
+    printf("1.view the list\n2.add new task\n3.delete a task\n4.quit\n");
+    int ch;
+    printf("enter an option:");
+    scanf("%d",&ch);
+    printf("\n");
+    switch(ch){
+    case 1:{
+        display();
+        break;
+    }
+    case 2:{
+        break;
+
+    }
+    case 3:{
+
+ break;
+    }
+    case 4:   { return 0;
+
+    } 
+    }
+  
 }
 
 
