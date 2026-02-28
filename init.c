575a2a51da1f8ac3dee3b6d56bef8e27152804ce
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-03-01 00:15:50 +0530

    corrected the logic mistake

--- init.c
+++ init.c
@@ -1,11 +1,11 @@
 //to create the files to get the app running 
 #include<stdlib.h>
 #include<stdio.h>
- 
+
 int main(){
 
     FILE *copy,*original;
-    if(fopen("./copy.txt","r")!=NULL || fopen("./original.txt","r")!=NULL){
+    if(fopen("./copy.txt","r")==NULL || fopen("./original.txt","r")==NULL){
     copy=fopen("./copy.txt","w");
     original=fopen("./original.txt","w");
     fclose(copy);
